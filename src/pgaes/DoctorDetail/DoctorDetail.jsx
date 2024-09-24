d0368ef6db74545e8872c47548bff92570a46fd3
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2024-09-24 22:18:59 +0500

    code

--- src/pgaes/DoctorDetail/DoctorDetail.jsx
+++ src/pgaes/DoctorDetail/DoctorDetail.jsx
@@ -7,27 +7,30 @@ export default function DoctorDetail() {
     const { doctor } = location.state;  // Access the passed doctor object
     console.log("doctor------", doctor)
     return (
-        <div className='docDetail'>
-            <div className="docImage">
-                <img src={doctor?.image} alt="" />
-            </div>
+        <div className="docs">
+            <div className='docDetail'>
+                <div className="docImage">
+                    <img src={doctor?.image} alt="" />
+                </div>
 
-            <div className="docDescription">
-                <div className="docname">
-                    <h1>{doctor?.name}</h1>
-                    <img src={tick_logo} alt="" />
+                <div className="docDescription">
+                    <div className="docname">
+                        <h1>{doctor?.name}</h1>
+                        <img src={tick_logo} alt="" />
+                    </div>
+                    <div className="deignation">
+                        <p>MBBS - General Physician</p>
+                        <div className="experince">{doctor?.experience}</div>
+                    </div>
+                    <div className="about">
+                        <p>About</p>
+                        <img src={expIcon} alt="" />
+                    </div>
+                    <p className='docAbout'>{doctor?.about}</p>
+                    <p className='appoint' > Appointment fee: <span className='highlightedText'>{doctor?.fees}</span></p>
                 </div>
-                <div className="deignation">
-                    <p>MBBS - General Physician</p>
-                    <div className="experince">{doctor?.experience}</div>
-                </div>
-                <div className="about">
-                    <p>About</p>
-                    <img src={expIcon} alt="" />
-                </div>
-                <p className='docAbout'>{doctor?.about}</p>
-                <p className='appoint' > Appointment fee: <span className='highlightedText'>{doctor?.fees}</span></p>
             </div>
+            <h3>Booking slots</h3>
         </div>
     )
 }
